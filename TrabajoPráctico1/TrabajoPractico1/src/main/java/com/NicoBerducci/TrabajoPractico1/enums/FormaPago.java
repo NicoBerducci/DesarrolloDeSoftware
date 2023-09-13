4a7b19b7228a3b40eb68e827dd780861561c48ce
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-09-13 18:36:29 -0300

    Delete TrabajoPráctico1/TrabajoPractico1 directory

--- TrabajoPráctico1/TrabajoPractico1/src/main/java/com/NicoBerducci/TrabajoPractico1/enums/FormaPago.java
+++
@@ -1,7 +0,0 @@
-package com.NicoBerducci.TrabajoPractico1.enums;
-
-    public enum FormaPago {
-        EFECTIVO,
-        ETC
-    }
-
--- TrabajoPráctico1/TrabajoPractico1/src/main/java/com/NicoBerducci/TrabajoPractico1/enums/Tipo.java
+++
@@ -1,6 +0,0 @@
-package com.NicoBerducci.TrabajoPractico1.enums;
-
-public enum Tipo{
-    Manufacturado,
-    Insumo
-}
